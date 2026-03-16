b31fa9c60d95e24e1de44a6dad126d244b8399f2
Adrian Cuschieri <cuschieri.adrian@gmail.com>
2026-03-16 18:47:25 +0100

    Adding a test function to add 2 numbers

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ func GetMessage() string {
     return "CI Build Successful: Go App is running!"
 }
 
+
+// Add takes two integers and returns their sum
+func Add(a int, b int) int {
+    return a + b
+}
+
 func main() {
     fmt.Println(GetMessage())
+    result := Add(5, 10)
+    fmt.Printf("The sum is: %d\n", result)
 }
--- main_test.go
+++ main_test.go
@@ -10,3 +10,27 @@ func TestGetMessage(t *testing.T) {
         t.Errorf("Expected %s but got %s", expected, actual)
     }
 }
+
+
+func TestAddTableDriven(t *testing.T) {
+    // Define a slice of anonymous structs for our test cases
+    tests := []struct {
+        name     string
+        a, b     int
+        expected int
+    }{
+        {"Positive numbers", 5, 5, 10},
+        {"Negative numbers", -1, -1, -2},
+        {"Mixing types", -5, 10, 5},
+        {"Zero case", 0, 0, 0},
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            result := Add(tt.a, tt.b)
+            if result != tt.expected {
+                t.Errorf("got %d, want %d", result, tt.expected)
+            }
+        })
+    }
+}
